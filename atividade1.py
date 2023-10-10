35c6573c42f55a4562f679c4c70d8dcdbbf2b3f1
Leofelixll <fleo16leo@gmail.com>
2023-10-10 16:55:39 -0300

    adicionado a palava digite e os kaomoji

--- atividade1.py
+++ atividade1.py
@@ -1,8 +1,8 @@
-nota_1 = float( input(" nota1: ") )
-nota_2 = float( input(" nota2: ") )
-nota_3 = float( input(" nota3: ") )
+nota_1 = float( input(" digite a nota1: ") )
+nota_2 = float( input(" digite a nota2: ") )
+nota_3 = float( input(" digite a nota3: ") )
 media = (nota_1+nota_2+nota_3)/3 
 if media >=7:
-    print('aprovado')
+    print('aprovadoヾ(≧▽≦*)o')
 else:
-    print ("reprovado")
+    print ("reprovado＞﹏＜")
